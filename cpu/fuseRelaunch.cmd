cc4aabd3b2542779dfc1d6f2ebb5e4bc77177d0a
iPavel92 <ipavel92@gmail.com>
2012-03-19 12:53:42 +0300

    InsructionRegister added

--- cpu/fuseRelaunch.cmd
+++ cpu/fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "D:/CPUVerilog/cpu/testImmediateSignExtend_isim_beh.exe" -prj "D:/CPUVerilog/cpu/testImmediateSignExtend_beh.prj" "work.testImmediateSignExtend" "work.glbl" 
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "D:/CPUVerilog/cpu/testInstructionRegister_isim_beh.exe" -prj "D:/CPUVerilog/cpu/testInstructionRegister_beh.prj" "work.testInstructionRegister" "work.glbl" 
